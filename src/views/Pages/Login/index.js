7492002bbb387bf93ac9e95e22bdd89c52d39a6b
user <mifjak1@gmail.com>
2019-04-11 08:37:28 +0700

    upload master react

---
+++ src/views/Pages/Login/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <h2>halaman login</h2>
+      </div>
+    );
+  }
+}
